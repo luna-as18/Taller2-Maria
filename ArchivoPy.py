d0cb8679f9e570739ae51717f6d85f345c41dc88
luna-as18 <85699323+luna-as18@users.noreply.github.com>
2024-08-14 19:01:32 -0500

    Funcion despedida

--- ArchivoPy.py
+++ ArchivoPy.py
@@ -1,3 +1,7 @@
 #Funcion que salude a una persona
 def saludar(nombre):
     return "Hola " + nombre + "!"
+
+#funcion que se despida de una persona
+def despedir(nombre):
+    return "Adios " + nombre + "!"
